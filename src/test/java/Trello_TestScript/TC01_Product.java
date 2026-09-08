88a4ddbd2c3a8224c814046cd42707e20d65aac2
Tanay Pande <318622588+tanaypixel23@users.noreply.github.com>
2026-09-08 19:17:50 +0530

    Added Partial Search for TC01

--- src/test/java/Trello_TestScript/TC01_Product.java
+++ src/test/java/Trello_TestScript/TC01_Product.java
@@ -3,6 +3,7 @@ package Trello_TestScript;
 import commonTest.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,12 +16,13 @@ import java.util.List;
 public class TC01_Product extends TestBase {
 
     @Test
-    public void verifySearchProduct() throws InterruptedException {
-        
-        driver.findElement(By.linkText("Products")).click();
+    public void verifySearchProduct() {
+
+        clickUntilLoaded(
+                By.linkText("Products")
+        );
         WebElement searchBox = driver.findElement(By.xpath("//input[@id='searchinput']"));
         searchBox.sendKeys("mascara");
-
         boolean found = false;
 
         List<WebElement> productCards =
@@ -37,8 +39,35 @@ public class TC01_Product extends TestBase {
                 break;
             }
         }
-
         Assert.assertTrue(found, "Mascara product was not found");
+    }
+
+    @Test
+    public void SearchPartialKeyword() {
+
+        clickUntilLoaded(
+                By.linkText("Products")
+        );
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='searchinput']"));
+        searchBox.sendKeys("eye");
+
+        boolean found = false;
+
+        List<WebElement> productCards =
+                driver.findElements(By.cssSelector("div.product-card"));
+
+        for (WebElement card : productCards) {
+
+            String title = card.findElement(
+                            By.cssSelector("h3.product-title"))
+                    .getText();
+
+            if (title.toLowerCase().contains("eye")) {
+                found = true;
+                break;
+            }
+        }
+        Assert.assertTrue(found, "Eye product was not found");
     }
 }
 
