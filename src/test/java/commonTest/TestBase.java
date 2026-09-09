48368b7bbdb28c7ebb2fcc95f250713707d1c96a
Samuel Baiju <samuel.baiju18@ibm.com>
2026-09-09 12:53:24 +0530

    Add TC06 cross-browser tests: checkout form, ES modules, responsive grid, mobile layout

--- src/test/java/commonTest/TestBase.java
+++ src/test/java/commonTest/TestBase.java
@@ -3,6 +3,7 @@ package commonTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
