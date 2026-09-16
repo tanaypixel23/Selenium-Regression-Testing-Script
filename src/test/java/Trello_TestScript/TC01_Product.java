4a3c476744f1459b4b65139184c1eecb34107439
Tanay Pande <318622588+tanaypixel23@users.noreply.github.com>
2026-09-16 13:28:19 +0530

    changed commonTest

--- src/test/java/Trello_TestScript/TC01_Product.java
+++ src/test/java/Trello_TestScript/TC01_Product.java
@@ -2,8 +2,6 @@ package Trello_TestScript;
 
 import commonTest.TestBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,14 +13,16 @@ import java.util.List;
 
 public class TC01_Product extends TestBase {
 
-    @Test
-    public void verifySearchProduct() {
+    public void SearchProduct(String productName) {
 
-        clickUntilLoaded(
-                By.linkText("Products")
-        );
-        WebElement searchBox = driver.findElement(By.xpath("//input[@id='searchinput']"));
-        searchBox.sendKeys("mascara");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Products"))).click();
+
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("searchinput")));
+
+        searchBox.clear();
+        searchBox.sendKeys(productName);
         boolean found = false;
 
         List<WebElement> productCards =
@@ -34,41 +34,33 @@ public class TC01_Product extends TestBase {
                             By.cssSelector("h3.product-title"))
                     .getText();
 
-            if (title.toLowerCase().contains("mascara")) {
+            if (title.toLowerCase().contains(productName.toLowerCase())) {
                 found = true;
                 break;
             }
         }
-        Assert.assertFalse(found, "Mascara product was not found");
+        Assert.assertTrue(found, productName + "product was not found");
     }
 
     @Test
     public void SearchPartialKeyword() {
-
-        clickUntilLoaded(
-                By.linkText("Products")
-        );
-        WebElement searchBox = driver.findElement(By.xpath("//input[@id='searchinput']"));
-        searchBox.sendKeys("eye");
-
-        boolean found = false;
-
-        List<WebElement> productCards =
-                driver.findElements(By.cssSelector("div.product-card"));
-
-        for (WebElement card : productCards) {
-
-            String title = card.findElement(
-                            By.cssSelector("h3.product-title"))
-                    .getText();
-
-            if (title.toLowerCase().contains("eye")) {
-                found = true;
-                break;
-            }
-        }
-        Assert.assertTrue(found, "Eye product was not found");
+        SearchProduct("Eye");
     }
+
+    @Test
+    public void SearchFullKeyword() {
+        SearchProduct("Mascara");
+    }
+
+    /*@Test
+    public void dummy() {
+        Assert.fail();
+    }
+    */
+
+
+
+
 }
 
 
--- src/test/java/commonTest/TestBase.java
+++ src/test/java/commonTest/TestBase.java
@@ -1,27 +1,96 @@
 package commonTest;
 
 import io.qameta.allure.Attachment;
-import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 
-import java.io.File;
-import java.io.IOException;
+import java.time.Duration;
 
 public class TestBase {
-    public static WebDriver driver;
-    public static File website;
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
-    public void setupdriver() throws InterruptedException {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    @Parameters("browser")
+    public void setupDriver(@Optional("chrome") String browser) {
+
+        boolean isGitHubActions = System.getenv("GITHUB_ACTIONS") != null;
+
+        switch (browser.toLowerCase()) {
+
+            case "firefox":
+
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                if (isGitHubActions) {
+                    firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--width=1920");
+                    firefoxOptions.addArguments("--height=1080");
+                }
+
+                DriverFactory.setDriver(new FirefoxDriver(firefoxOptions));
+                break;
+
+            case "edge":
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+
+                if (isGitHubActions) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--no-sandbox");
+                    edgeOptions.addArguments("--disable-dev-shm-usage");
+                    edgeOptions.addArguments("--disable-gpu");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                }
+
+                DriverFactory.setDriver(new EdgeDriver(edgeOptions));
+                break;
+
+            case "chrome":
+            default:
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                if (isGitHubActions) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
+
+                DriverFactory.setDriver(new ChromeDriver(chromeOptions));
+                break;
+        }
+
+        driver = DriverFactory.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        if (!isGitHubActions) {
+            driver.manage().window().maximize();
+        }
+
         driver.get("https://surajkumar-ibm.github.io/Selenium-Miniproject-Application/");
     }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+
 
     @Attachment(value = "Failure Screenshot",
             type = "image/png")
@@ -29,73 +98,6 @@ public class TestBase {
 
         return ((TakesScreenshot) driver)
                 .getScreenshotAs(OutputType.BYTES);
-    }
-
-    @BeforeSuite
-    public void cleanAllureResults() {
-
-        File folder = new File("target/allure-results");
-
-        if(folder.exists()) {
-
-            for(File file : folder.listFiles()) {
-                file.delete();
-            }
-        }
-    }
-
-    @AfterMethod
-    public void teardown(){
-        driver.quit();
-    }
-
-    public static void clickUntilLoaded(By clickLocator) {
-
-        for (int i = 0; i < 5; i++) {
-
-            driver.findElement(clickLocator).click();
-
-            try {
-                Thread.sleep(2000);
-
-                if (driver.findElements(By.cssSelector(".no-products")).isEmpty()) {
-                    return; // Success
-                }
-
-                System.out.println("Backend did not load, retrying again...");
-
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-
-        throw new RuntimeException("Backend did not load after 5 attempts.");
-    }
-
-    public String captureScreenshot(String testName) {
-
-        String folder =
-                System.getProperty("user.dir")
-                        + "/Screenshots";
-
-        new File(folder).mkdirs();
-
-        TakesScreenshot ts = (TakesScreenshot) driver;
-
-        File source =
-                ts.getScreenshotAs(OutputType.FILE);
-
-        String destination =
-                folder + "/" + testName + ".png";
-
-        try {
-            FileHandler.copy(source,
-                    new File(destination));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return destination;
     }
 
 }
